18a89423864a72079cbc08da5a116430ed2e9399
Ricardo Lafuente <r@manufacturaindependente.org>
2021-05-07 16:42:56 +0100

    Working static Nuxt version

--- signup-nuxt/nuxt.config.js
+++ signup-nuxt/nuxt.config.js
@@ -7,6 +7,20 @@ export default {
     // 'fork-awesome',
     '~/assets/css/main.css',
   ],
+  head: {
+    title: 'Beabee Playground',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'my website description'
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  target: 'static',
   build: { // https://github.com/nuxt/nuxt.js/issues/9224#issuecomment-830577523
     babel: {
       plugins: [['@babel/plugin-proposal-private-methods', { loose: true }]],
