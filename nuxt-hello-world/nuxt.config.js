d14e5da76074926afbfc7591558e50cfe4c2e7b1
Ricardo Lafuente <r@manufacturaindependente.org>
2021-05-10 13:37:08 +0100

    WIP middleware setup

--- nuxt-hello-world/nuxt.config.js
+++ nuxt-hello-world/nuxt.config.js
@@ -7,6 +7,9 @@ export default {
     // 'fork-awesome',
     '~/assets/css/main.css',
   ],
+  serverMiddleware: [
+    { path: "/server-middleware", handler: "~/server-middleware/rest.js" },
+  ],
   build: { // https://github.com/nuxt/nuxt.js/issues/9224#issuecomment-830577523
     babel: {
       plugins: [['@babel/plugin-proposal-private-methods', { loose: true }]],
